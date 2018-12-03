ff708b09a0214bff9a5b11027ba01ee01bc5697a
Paul Moore <pdmoore@gmail.com>
2018-12-02 20:54:31 -0800

    part 2 solution, need to refactor

--- src/day1.java
+++ src/day1.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class day1 {
@@ -34,6 +35,35 @@ public class day1 {
         }catch(Exception err){
             err.printStackTrace();
         }
+    }
 
+    public static int solution_2(String input) {
+        Collection<Integer> frequencies = new HashSet<>();
+//        List<Integer> frequencies = new ArrayList<>();
+        int sum = 0;
+        while (true) {
+            Scanner scanner = new Scanner(input);
+            while (scanner.hasNext()) {
+                sum += scanner.nextInt();
+                if (frequencies.contains(sum)) return sum;
+                frequencies.add(sum);
+            }
+        }
+    }
+
+    public static int solution_2_from_file() throws FileNotFoundException {
+        String puzzleInputFile = "data/aoc18.1.txt";
+
+        Collection<Integer> frequencies = new HashSet<>();
+        int sum = 0;
+        while (true) {
+            File f = new File(puzzleInputFile);
+            Scanner scanner = new Scanner(f);
+            while (scanner.hasNext()) {
+                sum += scanner.nextInt();
+                if (frequencies.contains(sum)) return sum;
+                frequencies.add(sum);
+            }
+        }
     }
 }
--- test/day1Tests.java
+++ test/day1Tests.java
@@ -1,18 +1,24 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class day1Tests {
 
     @Test
     public void parsePositiveLine() {
         int actual = day1.parseLine("+3");
-        Assertions.assertEquals(3, actual);
+        assertEquals(3, actual);
     }
 
     @Test
     public void parseNegativeLine() {
         int actual = day1.parseLine("-2");
-        Assertions.assertEquals(-2, actual);
+        assertEquals(-2, actual);
     }
 
     @Test
@@ -20,4 +26,21 @@ public class day1Tests {
         day1.solution_1();
     }
 
+    @Test
+    public void part2() {
+        String input = "+3 +3 +4 -2 -4";
+
+        assertEquals(10, day1.solution_2(input));
+
+        input = "-6 +3 +8 +5 -6";
+        assertEquals(5, day1.solution_2(input));
+
+        input = "+7 +7 -2 -7 -4";
+        assertEquals(14, day1.solution_2(input));
+    }
+
+    @Test
+    public void solution_2() throws Exception {
+        assertEquals(59908, day1.solution_2_from_file());
+    }
 }
