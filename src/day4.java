a868b86df8af79613a15fb313456dc6eaa097d81
Paul Moore <pdmoore@gmail.com>
2018-12-10 11:15:43 -0800

    day 4, soln 2 finished

--- src/day4.java
+++ src/day4.java
@@ -1,6 +1,167 @@
+import java.util.*;
+import java.util.spi.LocaleServiceProvider;
+
 public class day4 {
 
     public static int solution_1(String filename) {
-        return 0;
+        List<String> guardActivities = utilities.getFileContentsAsStrings(filename);
+        Collections.sort(guardActivities);
+
+        HashMap<Integer, GuardLog> guardLogsById = new HashMap<>();
+        createGuardLog(guardActivities, guardLogsById);
+
+        int sleepiestGuardId = findSleepiest(guardLogsById);
+        int sleepiestMinute = guardLogsById.get(sleepiestGuardId).sleepiestMinute();
+
+        return sleepiestGuardId * sleepiestMinute;
+    }
+
+    public static int solution_2(String filename) {
+        List<String> guardActivities = utilities.getFileContentsAsStrings(filename);
+        Collections.sort(guardActivities);
+
+        HashMap<Integer, GuardLog> guardLogsById = new HashMap<>();
+        createGuardLog(guardActivities, guardLogsById);
+
+        return sleepiestMinuteTimesGuard(guardLogsById);
+    }
+
+    private static void createGuardLog(List<String> guardActivities, HashMap<Integer, GuardLog> guardLogsById) {
+        int minuteSleepingStarts = 0;
+        GuardLog guardLog = new GuardLog(-999);
+
+        for (String activity :
+                guardActivities) {
+
+            if (activity.contains("begins shift")) {
+                // switch to tracking a new guards sleeping
+                int guardId = extractGuardId(activity);
+                if (guardLogsById.containsKey(guardId)) {
+                    guardLog = guardLogsById.get(guardId);
+                } else {
+                    guardLog = new GuardLog(guardId);
+                    guardLogsById.put(guardId, guardLog);
+                }
+            } else if (activity.contains("asleep")) {
+                minuteSleepingStarts = extractTimePortion(activity);
+            } else if (activity.contains("wakes")) {
+                int minuteSleepingEnds = extractTimePortion(activity);
+                guardLog.sleepSession(minuteSleepingStarts, minuteSleepingEnds);
+            }
+        }
+    }
+
+    private static int findSleepiest(HashMap<Integer, GuardLog> guardLogsById) {
+        int mostSleptCount = -1;
+        int sleepiestGuard = -1;
+
+        Iterator<Map.Entry<Integer, GuardLog>> iter = guardLogsById.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<Integer, GuardLog> guardLog = iter.next();
+            if (guardLog.getValue().totalsSlept > mostSleptCount) {
+                mostSleptCount = guardLog.getValue().totalsSlept;
+                sleepiestGuard = guardLog.getKey().intValue();
+            }
+        }
+
+        return sleepiestGuard;
+    }
+
+    private static int sleepiestMinuteTimesGuard(HashMap<Integer, GuardLog> guardLogsById) {
+        int sleepiestMinute = -1;
+        int sleepiestCountOfMinute = -1;
+        int guardId = -1;
+
+        Iterator<Map.Entry<Integer, GuardLog>> iter = guardLogsById.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<Integer, GuardLog> guardLogEntry = iter.next();
+            GuardLog guardLog = guardLogEntry.getValue();
+
+            // find sleepiest minute count of this guard
+            // if it's more than saved, then save guardId and the minute index
+            if (guardLog.largestSleepCount() > sleepiestCountOfMinute) {
+                guardId = guardLog.Id;
+                sleepiestMinute = guardLog.sleepiestMinute();
+                sleepiestCountOfMinute = guardLog.largestSleepCount();
+            }
+
+        }
+
+        return guardId * sleepiestMinute;
+    }
+
+
+
+
+    public static int extractGuardId(String line) {
+        int hashIndex = line.indexOf('#');
+        int nextSpaceIndex = line.indexOf(' ', hashIndex);
+        int guardId = Integer.parseInt(line.substring(hashIndex + 1, nextSpaceIndex));
+        return guardId;
+    }
+
+    public static int extractTimePortion(String line) {
+        int firstColon = line.indexOf(':');
+        String minutesPortion = line.substring(firstColon + 1, firstColon + 3);
+        int minutes = Integer.parseInt(minutesPortion);
+        return minutes;
+    }
+
+    public static GuardLog singleGuardTracking(List<String> activities) {
+        HashMap<Integer, GuardLog> guardLogsById = new HashMap<>();
+        createGuardLog(activities, guardLogsById);
+
+        return guardLogsById.entrySet().iterator().next().getValue();
+    }
+
+    public static class GuardLog {
+        public int Id;
+        int totalsSlept;
+        int largestSleepCount;
+        HashMap<Integer, Integer> minutesMap = new HashMap<>();
+
+        public GuardLog(int id) {
+            Id = id;
+            for (int i = 0; i < 60; i++) {
+                minutesMap.put(i, 0);
+            }
+        }
+
+        public void addSleepMinutes(int minutesAsleep) {
+            totalsSlept += minutesAsleep;
+        }
+
+        public void sleepSession(int starts, int ends) {
+            addSleepMinutes(ends - starts);
+            addOneForEachMinuteSlept(starts, ends);
+        }
+
+        private void addOneForEachMinuteSlept(int starts, int ends) {
+            for (int i = starts; i < ends; i++) {
+                int count = minutesMap.get(i);
+                count++;
+                minutesMap.put(i, count);
+                if (count > largestSleepCount) {
+                    largestSleepCount = count;
+                }
+            }
+        }
+
+        public int sleepiestMinute() {
+            int sleepiestMinute = -1;
+            int mostSlept = -1;
+            for (int i = 0; i < minutesMap.size(); i++) {
+                int count = minutesMap.get(i);
+                if (count > mostSlept) {
+                    sleepiestMinute = i;
+                    mostSlept = count;
+                }
+            }
+            return sleepiestMinute;
+        }
+
+        public int largestSleepCount() {
+            return largestSleepCount;
+        }
     }
 }
--- test/day4Tests.java
+++ test/day4Tests.java
@@ -1,4 +1,8 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -6,7 +10,58 @@ public class day4Tests {
 
     @Test
     public void solution_1_testFile() {
-        assertEquals(999, day4.solution_1("data/aoc18.4.txt"));
+        assertEquals(94542, day4.solution_1("data/aoc18.4.txt"));
+    }
+
+    @Test
+    public void solution_2_testFile() {
+        assertEquals(50966, day4.solution_2("data/aoc18.4.txt"));
+    }
+
+    @Test
+    public void extractGuardId() {
+        assertEquals(2797, day4.extractGuardId("[1518-02-13 23:47] Guard #2797 begins shift"));
+    }
+
+    @Test
+    public void ParseTimePortion() {
+        assertEquals(5, day4.extractTimePortion("[1518-11-01 00:05] falls asleep"));
+    }
+
+    @Test
+    public void singleGuard_SingleSleep() {
+        List<String> activities = Arrays.asList(
+                "[1518-11-01 00:00] Guard #10 begins shift",
+                "[1518-11-01 00:05] falls asleep",
+                "[1518-11-01 00:25] wakes up",
+                "[1518-11-03 00:24] falls asleep",
+                "[1518-11-03 00:29] wakes up",
+                "[1518-11-01 00:30] falls asleep",
+                "[1518-11-01 00:55] wakes up");
+
+        day4.GuardLog actual = day4.singleGuardTracking(activities);
+        assertEquals(50, actual.totalsSlept);
+        assertEquals(24, actual.sleepiestMinute());
+    }
+
+    @Test
+    public void singleGuard_Solution2() {
+        List<String> activities = Arrays.asList(
+                "[1518-11-01 23:58] Guard #99 begins shift",
+                "[1518-11-02 00:40] falls asleep",
+                "[1518-11-02 00:50] wakes up",
+                "[1518-11-04 00:02] Guard #99 begins shift",
+                "[1518-11-04 00:36] falls asleep",
+                "[1518-11-04 00:46] wakes up",
+                "[1518-11-05 00:03] Guard #99 begins shift",
+                "[1518-11-05 00:45] falls asleep",
+                "[1518-11-05 00:55] wakes up"
+        );
+        day4.GuardLog actual = day4.singleGuardTracking(activities);
+        assertEquals(45, actual.sleepiestMinute());
+
+        //assert the count of 45 is whatever (3)
+        assertEquals(3, actual.largestSleepCount());
     }
 
 
