0de68e29fed0b6bdc6adb172a316f0b20a6ab2c1
Paul Moore <pdmoore@gmail.com>
2018-12-13 10:46:56 -0800

    day 13, solution 1

---
+++ src/day13.java
@@ -0,0 +1,212 @@
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class day13 {
+
+    private static int tick = 0;
+
+    public static class Cart {
+        public Direction orientation;
+        public int x;
+        public int y;
+        public int currentTick = 0;
+        public int intersections = 0;
+
+        public Cart(int y, int x, Direction orientation) {
+            this.x = x;
+            this.y = y;
+            this.orientation = orientation;
+        }
+    }
+
+
+    enum Direction {
+        UP, DOWN, LEFT, RIGHT,
+    };
+
+    enum IntersectionOption {
+        LEFT, STRAIGHT, RIGHT,
+    };
+
+    public static char[][] convertInputToMap(List<String> inputAsStrings) {
+        int rowCount = inputAsStrings.size();
+        int colCount = inputAsStrings.get(0).length();
+        char[][] map = new char[rowCount][colCount];
+        int i = 0;
+        for (String line :
+                inputAsStrings) {
+            map[i] = line.toCharArray();
+            i++;
+        }
+        return map;
+    }
+
+    public static List<Cart> extractCartsFromMap(char[][] map) {
+        List<Cart> carts = new ArrayList<>();
+
+        for (int y = 0; y < map[0].length; y++) {
+            for (int x = 0; x < map.length; x++) {
+
+                if (isCart(map[x][y])) {
+                    carts.add(createCart(map, x, y));
+                    removeCartCharacter(map, x, y);
+                }
+            }
+        }
+        return carts;
+    }
+
+    private static void removeCartCharacter(char[][] map, int x, int y) {
+        char cartCharacter = map[x][y];
+        switch (cartCharacter) {
+            case '>':
+            case '<':
+                map[x][y] = '-';
+                break;
+            case '^':
+            case 'v':
+                map[x][y] = '|';
+                break;
+        }
+    }
+
+    private static Cart createCart(char[][] map, int x, int y) {
+        char cartCharacter = map[x][y];
+        Direction orientation = null;
+        switch (cartCharacter) {
+            case '>':
+                orientation = Direction.RIGHT;
+                break;
+            case '<':
+                orientation = Direction.LEFT;
+                break;
+            case '^':
+                orientation = Direction.UP;
+                break;
+            case 'v':
+                orientation = Direction.DOWN;
+                break;
+        }
+        Cart cart = new Cart(x, y, orientation);
+        return cart;
+    }
+
+    private static boolean isCart(char c) {
+        String cartChars = "><^v";
+        return cartChars.indexOf(c, 0) != -1;
+    }
+
+    public static void tick(char[][] map, List<Cart> carts) {
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[0].length; x++) {
+
+                Cart cart = isThereACartAt(x, y, carts);
+                if (cart != null) {
+                    cartTick(map, cart);
+
+                    areThereTwoCartsAt(cart.x, cart.y, carts);
+                }
+            }
+        }
+
+        tick++;
+    }
+
+    private static Cart isThereACartAt(int x, int y, List<Cart> carts) {
+        for (Cart cart :
+                carts) {
+            if (cart.x == x && cart.y == y && cart.currentTick < tick) {
+                return cart;
+            }
+        }
+        return null;
+    }
+
+    private static void areThereTwoCartsAt(int x, int y, List<Cart> carts) {
+        int cartDetectedAt = 0;
+        for (Cart cart :
+                carts) {
+            if (cart.x == x && cart.y == y) {
+                cartDetectedAt++;
+
+                if (cartDetectedAt == 2) {
+                    throw new RuntimeException("boom - x: " + x + "  y: " + y);
+                }
+            }
+        }
+    }
+
+
+    private static void cartTick(char[][] map, Cart cart) {
+        if (cart.currentTick > tick) return;
+
+        cart.currentTick++;
+
+        if (cart.orientation == Direction.RIGHT) {
+            cart.x++;
+        } else if (cart.orientation == Direction.LEFT) {
+            cart.x--;
+        } else if (cart.orientation == Direction.DOWN) {
+            cart.y++;
+        } else if (cart.orientation == Direction.UP) {
+            cart.y--;
+        }
+
+        char newLocation = map[cart.y][cart.x];
+
+        // check if this new cell is a / or \ and change orientation (depends on current orientation!)
+        if (newLocation == '\\') {
+            backSlashAction(cart);
+        } else if (newLocation == '/') {
+            forwardSlashAction(cart);
+        } else if (newLocation == '+') {
+            intersectionAction(cart);
+        }
+    }
+
+    private static void backSlashAction(Cart cart) {
+        if (cart.orientation == Direction.RIGHT) {
+            cart.orientation = Direction.DOWN;
+        } else if (cart.orientation == Direction.LEFT) {
+            cart.orientation = Direction.UP;
+        } else if (cart.orientation == Direction.UP) {
+            cart.orientation = Direction.LEFT;
+        } else if (cart.orientation == Direction.DOWN) {
+            cart.orientation = Direction.RIGHT;
+        }
+    }
+
+    private static void forwardSlashAction(Cart cart) {
+        if (cart.orientation == Direction.RIGHT) {
+            cart.orientation = Direction.UP;
+        } else if (cart.orientation == Direction.LEFT) {
+            cart.orientation = Direction.DOWN;
+        } else if (cart.orientation == Direction.UP) {
+            cart.orientation = Direction.RIGHT;
+        } else if (cart.orientation == Direction.DOWN) {
+            cart.orientation = Direction.LEFT;
+        }
+    }
+
+    public static void intersectionAction(Cart cart) {
+
+        IntersectionOption option = IntersectionOption.values()[cart.intersections % 3];
+        if (cart.orientation == Direction.RIGHT) {
+            if (option == IntersectionOption.LEFT) cart.orientation = Direction.UP;
+            else if (option == IntersectionOption.RIGHT) cart.orientation = Direction.DOWN;
+        } else if (cart.orientation == Direction.LEFT) {
+            if (option == IntersectionOption.LEFT) cart.orientation = Direction.DOWN;
+            else if (option == IntersectionOption.RIGHT) cart.orientation = Direction.UP;
+        } else if (cart.orientation == Direction.UP) {
+            if (option == IntersectionOption.LEFT) cart.orientation = Direction.LEFT;
+            else if (option == IntersectionOption.RIGHT) cart.orientation = Direction.RIGHT;
+        } else if (cart.orientation == Direction.DOWN) {
+            if (option == IntersectionOption.LEFT) cart.orientation = Direction.RIGHT;
+            else if (option == IntersectionOption.RIGHT) cart.orientation = Direction.LEFT;
+        }
+
+        cart.intersections++;
+    }
+}
---
+++ test/day13Tests.java
@@ -0,0 +1,199 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class day13Tests {
+
+    // X read map and store in data structure
+    // X replace cart with appropriate section of track
+    // update map row by row, col within row.
+    // locate a cart within map, direction, and next move state
+    // Detect collision and halt as soon as one occurs - return collision location
+
+    @Test
+    public void buildMap_example() {
+        String filename = "data/aoc18.13a.txt";
+        List<String> inputAsStrings = utilities.getFileContentsAsStrings(filename);
+        char[][] actual = day13.convertInputToMap(inputAsStrings);
+
+        char[][] expected = {
+                { '/',  '-', '>',  '-', '\\', ' ', ' ', ' ',  ' ', ' ',  ' ', ' ', ' '},
+                { '|',  ' ', ' ',  ' ', ' ',  ' ', ' ', '/',  '-', '-',  '-', '-', '\\'},
+                { '|',  ' ', '/',  '-', '+',  '-', '-', '+',  '-', '\\', ' ', ' ', '|'},
+                { '|',  ' ', '|',  ' ', '|',  ' ', ' ', '|',  ' ', 'v',  ' ', ' ', '|'},
+                { '\\', '-', '+',  '-', '/',  ' ', ' ', '\\', '-', '+',  '-', '-', '/'},
+                { ' ',  ' ', '\'', '-', '-',  '-', '-', '-',  '-', '\\', ' ', ' ', ' '}
+        };
+
+        assertEquals(expected.length, actual.length);
+    }
+
+    @Test
+    public void BuildCartList_FromInitialMap() {
+        String filename = "data/aoc18.13a.txt";
+        List<String> inputAsStrings = utilities.getFileContentsAsStrings(filename);
+        char[][] map = day13.convertInputToMap(inputAsStrings);
+
+        List<day13.Cart> actual = day13.extractCartsFromMap(map);
+
+        day13.Cart expected_cart_1 = new day13.Cart(0, 2, day13.Direction.RIGHT);
+        day13.Cart expected_cart_2 = new day13.Cart(3, 9, day13.Direction.DOWN);
+
+        assertEquals(2, actual.size());
+
+        assertEquals(expected_cart_1.x, actual.get(0).x);
+        assertEquals(expected_cart_1.y, actual.get(0).y);
+        assertEquals(expected_cart_1.orientation, actual.get(0).orientation);
+
+        assertEquals(expected_cart_2.x, actual.get(1).x);
+        assertEquals(expected_cart_2.y, actual.get(1).y);
+        assertEquals(expected_cart_2.orientation, actual.get(1).orientation);
+    }
+
+    @Test
+    public void RemoveCartsFromMap() {
+        String filename = "data/aoc18.13a.txt";
+        List<String> inputAsStrings = utilities.getFileContentsAsStrings(filename);
+        char[][] map = day13.convertInputToMap(inputAsStrings);
+
+        List<day13.Cart> actual = day13.extractCartsFromMap(map);
+
+        assertEquals('-', map[0][2]);
+        assertEquals('|', map[3][9]);
+    }
+
+    @Test
+    public void intersectionAction_startedRight() {
+        day13.Cart sut = new day13.Cart(0, 0, day13.Direction.RIGHT);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.UP, sut.orientation);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.UP, sut.orientation);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.RIGHT, sut.orientation);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.UP, sut.orientation);
+    }
+
+    @Test
+    public void intersection_startedLeft() {
+        day13.Cart sut = new day13.Cart(0, 0, day13.Direction.LEFT);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.DOWN, sut.orientation);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.DOWN, sut.orientation);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.LEFT, sut.orientation);
+    }
+
+    @Test
+    public void intersectionAction_startedUp() {
+        day13.Cart sut = new day13.Cart(0, 0, day13.Direction.UP);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.LEFT, sut.orientation);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.LEFT, sut.orientation);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.UP, sut.orientation);
+    }
+
+    @Test
+    public void intersectionAction_startedDown() {
+        day13.Cart sut = new day13.Cart(0, 0, day13.Direction.DOWN);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.RIGHT, sut.orientation);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.RIGHT, sut.orientation);
+
+        day13.intersectionAction(sut);
+        assertEquals(day13.Direction.DOWN, sut.orientation);
+    }
+
+    @Test
+    public void ManyTicks_MoveRight_MoveDownIntersection() {
+        String filename = "data/aoc18.13a.txt";
+        List<String> inputAsStrings = utilities.getFileContentsAsStrings(filename);
+        char[][] map = day13.convertInputToMap(inputAsStrings);
+
+        List<day13.Cart> carts = day13.extractCartsFromMap(map);
+
+        day13.tick(map, carts);
+
+        day13.Cart expected_cart_1 = new day13.Cart(0, 3,  day13.Direction.RIGHT);
+        day13.Cart expected_cart_2 = new day13.Cart(4, 9, day13.Direction.RIGHT);
+
+        assertEquals(expected_cart_1.x, carts.get(0).x);
+        assertEquals(expected_cart_1.y, carts.get(0).y);
+        assertEquals(expected_cart_1.orientation, carts.get(0).orientation);
+
+        assertEquals(expected_cart_2.x, carts.get(1).x);
+        assertEquals(expected_cart_2.y, carts.get(1).y);
+        assertEquals(expected_cart_2.orientation, carts.get(1).orientation);
+
+        // Next tick the first cart hits a \
+        day13.tick(map, carts);
+
+        expected_cart_1 = new day13.Cart(0, 4,  day13.Direction.DOWN);
+        assertEquals(expected_cart_1.x, carts.get(0).x);
+        assertEquals(expected_cart_1.y, carts.get(0).y);
+        assertEquals(expected_cart_1.orientation, carts.get(0).orientation);
+
+        expected_cart_2 = new day13.Cart(4, 10, day13.Direction.RIGHT);
+        assertEquals(expected_cart_2.x, carts.get(1).x);
+        assertEquals(expected_cart_2.y, carts.get(1).y);
+        assertEquals(expected_cart_2.orientation, carts.get(1).orientation);
+
+        day13.tick(map, carts);     // nothing interesting - cart1 moves down, cart2 moves right
+
+        day13.tick(map, carts);
+
+        expected_cart_1 = new day13.Cart(2, 4,  day13.Direction.RIGHT);
+        assertEquals(expected_cart_1.x, carts.get(0).x);
+        assertEquals(expected_cart_1.y, carts.get(0).y);
+        assertEquals(expected_cart_1.orientation, carts.get(0).orientation);
+
+        expected_cart_2 = new day13.Cart(4, 12, day13.Direction.UP);
+        assertEquals(expected_cart_2.x, carts.get(1).x);
+        assertEquals(expected_cart_2.y, carts.get(1).y);
+        assertEquals(expected_cart_2.orientation, carts.get(1).orientation);
+
+    }
+
+
+    @Test
+    public void day13_solution1() {
+        String filename = "data/aoc18.13.txt";
+        List<String> inputAsStrings = utilities.getFileContentsAsStrings(filename);
+        char[][] map = day13.convertInputToMap(inputAsStrings);
+
+        List<day13.Cart> carts = day13.extractCartsFromMap(map);
+
+        int i = 1;
+        while (true) {
+            try {
+                day13.tick(map, carts);
+            } catch (Exception e) {
+                assertEquals("SOLUTION", e.getMessage());
+            }
+        }
+    }
+
+    private String dumpCart(day13.Cart cart) {
+        return String.format("y: %4d   x: %4d   dir: %s\n", cart.y, cart.x, cart.orientation);
+    }
+
+}
