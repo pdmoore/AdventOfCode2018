8fe28db81d184e6f92e183c0f0d25ec1af786fa8
Paul Moore <pdmoore@gmail.com>
2018-12-18 10:29:36 -0800

    day 7  pom 1

--- src/day7.java
+++ src/day7.java
@@ -1,2 +1,31 @@
+import java.util.List;
+import java.util.Map;
+
 public class day7 {
+    public static String solution1(String filename) {
+        // read file as strings
+
+        // build dependencies from string list
+
+        // calculate step order
+
+
+
+        return null;
+    }
+
+    public static String calculateStepOrder(Map<String, List<String>> rules) {
+        // while there are rules remaining
+        
+        // loop through keys, find a key with null list
+
+        // add that key the running step order
+
+        // remove that key/value from the rules
+
+        // remove that key from all remaining lists
+
+
+        return null;
+    }
 }
--- test/day7Tests.java
+++ test/day7Tests.java
@@ -1,3 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
 public class day7Tests {
 
     // parse a line to get a rule
@@ -5,4 +11,37 @@ public class day7Tests {
     // process rules
 
     // print result
+
+
+    @Test
+    @Disabled
+    public void solution1_example() {
+        String filename = "data/aoc18.7a.txt";
+
+        String actual = day7.solution1(filename);
+
+        Assertions.assertEquals("CABDFE", actual);
+    }
+
+    @Test
+    public void calculateStepOrder() {
+        Map<String, List<String>> rules = new HashMap<>();
+        List<String> a_depends = Arrays.asList("C");
+        rules.put("A", a_depends);
+        List<String> b_depends = Arrays.asList("A");
+        rules.put("B", b_depends);
+//        List<String> c_depends = Arrays.asList(null);
+        rules.put("C", Collections.EMPTY_LIST);
+        List<String> d_depends = Arrays.asList("A");
+        rules.put("D", d_depends);
+        List<String> e_depends = Arrays.asList("B", "D", "F");
+        rules.put("E", e_depends);
+        List<String> f_depends = Arrays.asList("C");
+        rules.put("F", f_depends);
+
+        String actual = day7.calculateStepOrder(rules);
+
+        Assertions.assertEquals("CABDFE", actual);
+    }
+
 }
