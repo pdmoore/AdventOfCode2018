4928967c664b2746f8fb1ceb1db3c0a7d8fe11aa
Paul Moore <pdmoore@gmail.com>
2018-12-17 22:56:05 -0800

    day 17, solution 2 - wip

--- src/day17.java
+++ src/day17.java
@@ -23,7 +23,7 @@ public class day17 {
                 } else if (acre == '#') {
                     boolean hasTreeAndLumber = isAdjacentToLumberyardAndTrees(input, row, col);
                     if (!hasTreeAndLumber)
-                    itBecomes = '.';
+                        itBecomes = '.';
                 }
 
                 nextGeneration[row][col] = itBecomes;
@@ -75,26 +75,23 @@ public class day17 {
         Collection<Character> acres = new ArrayList();
         for (int row = 0; row < input.length; row++) {
             for (int col = 0; col < input[0].length; col++) {
-                  acres.add(input[row][col]);
+                acres.add(input[row][col]);
             }
         }
 
         int treeCount = Collections.frequency(acres, '|');
         int lumberyardCount = Collections.frequency(acres, '#');
+        int result = treeCount * lumberyardCount;
 
-        return treeCount * lumberyardCount;
+        return result;
     }
 
-    public static int solution1(String filename) {
-        // read file
+    public static int solution1(String filename, int reps) {
         List<String> fileContentsAsStrings = utilities.getFileContentsAsStrings(filename);
         char[][] currentMap = utilities.convertInputToMap(fileContentsAsStrings);
 
-
-
-        // do 1..10 reps
         char[][] nextMap = new char[currentMap.length][currentMap[0].length];
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= reps; i++) {
             nextMap = generateNextMap(currentMap);
 
             for (int row = 0; row < nextMap.length; row++) {
@@ -106,4 +103,43 @@ public class day17 {
 
         return result(nextMap);
     }
+
+    public static int solution2(String filename, int reps) {
+        List<String> fileContentsAsStrings = utilities.getFileContentsAsStrings(filename);
+        char[][] currentMap = utilities.convertInputToMap(fileContentsAsStrings);
+
+        System.out.println("step  - trees   lumber   result   ");
+
+        char[][] nextMap = new char[currentMap.length][currentMap[0].length];
+        for (int i = 1; i <= reps; i++) {
+            nextMap = generateNextMap(currentMap);
+
+            for (int row = 0; row < nextMap.length; row++) {
+                for (int col = 0; col < nextMap[0].length; col++) {
+                    currentMap[row][col] = nextMap[row][col];
+                }
+            }
+
+            result(i, nextMap);
+
+        }
+
+        return result(nextMap);
+    }
+
+    private static void result(int rep, char[][] input) {
+        Collection<Character> acres = new ArrayList();
+        for (int row = 0; row < input.length; row++) {
+            for (int col = 0; col < input[0].length; col++) {
+                acres.add(input[row][col]);
+            }
+        }
+
+        int treeCount = Collections.frequency(acres, '|');
+        int lumberyardCount = Collections.frequency(acres, '#');
+        int result = treeCount * lumberyardCount;
+
+        System.out.println(String.format("%5d - %5d   %5d   %5d ", rep, treeCount, lumberyardCount, result));
+    }
 }
+
--- test/day17Tests.java
+++ test/day17Tests.java
@@ -90,7 +90,7 @@ public class day17Tests {
         // pass in example file name
         String filename = "data/aoc18.17a.txt";
 
-        int actual = day17.solution1(filename);
+        int actual = day17.solution1(filename, 10);
 
         int expected = 37 * 31;
         assertEquals(expected, actual);
@@ -101,9 +101,24 @@ public class day17Tests {
         // pass in example file name
         String filename = "data/aoc18.17.txt";
 
-        int actual = day17.solution1(filename);
+        int actual = day17.solution1(filename, 10);
 
         int expected = 543312;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Disabled
+    public void solution2() {
+        // There is a cycle starting at rep 439 and runs 28 cycles (467 repeats the next cycle).
+        // used excel to get the right answer - need to clean up the code to generate the result for soln2
+
+
+        String filename = "data/aoc18.17.txt";
+
+        int actual = day17.solution2(filename, 2000);
+
+        int expected = 199064;
         assertEquals(expected, actual);
     }
 }
