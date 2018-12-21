842a09ab6f5830539468a7d0532308b417dc41fd
Paul Moore <pdmoore@gmail.com>
2018-12-21 09:04:44 -0800

    day 8 solution 2

--- src/day8.java
+++ src/day8.java
@@ -17,12 +17,11 @@ public class day8 {
     }
 
     private static void parseInput(List<String> tokens) {
-        // while there are elements left
         if (tokens.isEmpty()) return;
 
         int numChildNodes = Integer.parseInt(tokens.get(0));
         tokens.remove(0);
-        int numMetadata  = Integer.parseInt(tokens.get(0));
+        int numMetadata = Integer.parseInt(tokens.get(0));
         tokens.remove(0);
 
         for (int i = 0; i < numChildNodes; i++) {
@@ -43,5 +42,96 @@ public class day8 {
         String input = utilities.fileAsString(inputFile);
 
         return sumMetadata(input);
+    }
+
+    public static int solution2(String inputFile) {
+        String input = utilities.fileAsString(inputFile);
+
+        return valueOfNode(input);
+    }
+
+
+    public static int valueOfNode(String input) {
+        String[] tokenItems = input.split(" ");
+        List<String> tokens = new ArrayList<>(Arrays.asList(tokenItems));
+
+        Node root = buildTree(null, tokens);
+
+//        return valueOfNode(root);
+        return root.value;
+    }
+
+    private static Node buildTree(Node parent, List<String> tokens) {
+        if (tokens.isEmpty()) return null;
+
+        int numChildNodes = Integer.parseInt(tokens.get(0));
+        tokens.remove(0);
+
+        int numMetadata = Integer.parseInt(tokens.get(0));
+        tokens.remove(0);
+
+        Node node = new Node();
+        List<Node> children = new ArrayList<>();
+        for (int i = 0; i < numChildNodes; i++) {
+            children.add(buildTree(node, tokens));
+        }
+        node.children = children;
+
+        List<String> metadata = new ArrayList<>();
+        for (int i = 0; i < numMetadata; i++) {
+            String metadataValue = tokens.get(0);
+            tokens.remove(0);
+            metadata.add(metadataValue);
+        }
+        node.metadata = metadata;
+        node.computeValue();
+
+        return node;
+    }
+
+//    private static int valueOfNode(Node node) {
+//        int value = 0;
+//
+//        for (int i = 0; i < node.metadata.size(); i++) {
+//            int childIndex = Integer.parseInt(node.metadata.get(i));
+//            if (childIndex < node.children.size()) {
+//                value += node.children.get(childIndex - 1).value();
+//            }
+//        }
+//
+//        return value;
+//    }
+
+    private static class Node {
+        Node parent;
+        List<String> metadata;
+        List<Node> children;
+        int value;
+
+//        public int value() {
+//            int value = 0;
+//            for (int i = 0; i < metadata.size(); i++) {
+//                value += Integer.parseInt(metadata.get(i));
+//            }
+//
+//            return value;
+//        }
+//
+        public void computeValue() {
+            value = 0;
+
+            if (children.isEmpty()) {
+                for (int i = 0; i < metadata.size(); i++) {
+                    value += Integer.parseInt(metadata.get(i));
+                }
+            } else {
+                for (int i = 0; i < metadata.size(); i++) {
+                    int childIndex = Integer.parseInt(metadata.get(i));
+                    if (childIndex <= children.size()) {
+                        value += children.get(childIndex - 1).value;
+                    }
+                }
+            }
+        }
     }
 }
--- test/day8Tests.java
+++ test/day8Tests.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,5 +18,29 @@ public class day8Tests {
         String inputFile = "data/aoc18.8.txt";
         int actual = day8.solution1(inputFile);
         assertEquals(48496, actual);
+    }
+
+    @Test
+    public void solution2_example() {
+        String input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+
+        int actual = day8.valueOfNode(input);
+        assertEquals(66, actual);
+    }
+
+    @Test
+    public void solution2_example2_deeperNesting() {
+        String input = "3 4 0 3 1 2 5 2 2 1 1 0 2 3 3 1 0 3 1 2 4 1 2 1 2 0 1 2 1 2 1 1 2 3";
+
+        int actual = day8.valueOfNode(input);
+        assertEquals(31, actual);
+    }
+
+    @Test
+    public void solution2() {
+        String inputFile = "data/aoc18.8.txt";
+        int actual = day8.solution2(inputFile);
+
+        assertEquals(-99, actual);
     }
 }
