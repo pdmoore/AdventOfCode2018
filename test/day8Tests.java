7622c252d72525de09ab9cec5db589ed9691bed3
Paul Moore <pdmoore@gmail.com>
2018-12-20 21:09:45 -0800

    day 8 initial impound

---
+++ test/day8Tests.java
@@ -0,0 +1,9 @@
+public class day8Tests {
+
+    // solve the example
+    // may not need to build data structure?
+
+    // solve soln 1 - read file as single string
+    // apply example algo to the string
+
+}
