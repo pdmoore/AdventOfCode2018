9569da0b6a1ea2b97cdc79e2adda2e8756f4b6ff
Paul Moore <pdmoore@gmail.com>
2018-12-20 21:34:10 -0800

    day 8 solution 1, no tree built

--- src/day8.java
+++ src/day8.java
@@ -1,2 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class day8 {
+    private static int sumOfMetadata;
+
+    public static int sumMetadata(String input) {
+        String[] tokenItems = input.split(" ");
+        List<String> tokens = new ArrayList<>(Arrays.asList(tokenItems));
+
+        sumOfMetadata = 0;
+        parseInput(tokens);
+
+
+        return sumOfMetadata;
+    }
+
+    private static void parseInput(List<String> tokens) {
+        // while there are elements left
+        if (tokens.isEmpty()) return;
+
+        int numChildNodes = Integer.parseInt(tokens.get(0));
+        tokens.remove(0);
+        int numMetadata  = Integer.parseInt(tokens.get(0));
+        tokens.remove(0);
+
+        for (int i = 0; i < numChildNodes; i++) {
+            parseInput(tokens);
+        }
+        processMetadataForNode(tokens, numMetadata);
+    }
+
+    private static void processMetadataForNode(List<String> tokens, int numMetadata) {
+        for (int i = 0; i < numMetadata; i++) {
+            int metadataValue = Integer.parseInt(tokens.get(0));
+            tokens.remove(0);
+            sumOfMetadata += metadataValue;
+        }
+    }
+
+    public static int solution1(String inputFile) {
+        String input = utilities.fileAsString(inputFile);
+
+        return sumMetadata(input);
+    }
 }
--- test/day8Tests.java
+++ test/day8Tests.java
@@ -1,9 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class day8Tests {
 
-    // solve the example
-    // may not need to build data structure?
+    @Test
+    public void solution1_example() {
+        String input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
 
-    // solve soln 1 - read file as single string
-    // apply example algo to the string
+        int actual = day8.sumMetadata(input);
+        assertEquals(138, actual);
+    }
 
+    @Test
+    public void solution1() {
+        String inputFile = "data/aoc18.8.txt";
+        int actual = day8.solution1(inputFile);
+        assertEquals(48496, actual);
+    }
 }
