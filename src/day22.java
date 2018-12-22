edb5b6733f0a7487fe4e1aad6acd06774babb520
Paul Moore <pdmoore@gmail.com>
2018-12-22 12:06:38 -0800

    day 22 calc risk level

--- src/day22.java
+++ src/day22.java
@@ -1,2 +1,23 @@
+import java.awt.*;
+
 public class day22 {
+
+    public static final char ROCKY  = '.';
+    public static final char WET    = '=';
+    public static final char NARROW = '|';
+
+    
+    public static int calculateRiskLevel(char[][] caveSystem, Point target) {
+        int riskLevel = 0;
+        for (int y = 0; y <= target.y; y++) {
+            for (int x = 0; x <= target.x; x++) {
+                switch (caveSystem[x][y]) {
+                    case WET:    riskLevel += 1; break;
+                    case NARROW: riskLevel += 2; break;
+                }
+            }
+        }
+
+        return riskLevel;
+    }
 }
--- test/day22Tests.java
+++ test/day22Tests.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.List;
+
 public class day22Tests {
+
+
+
+    @Test
+    public void calculateRiskLevel() {
+        String filename = "data/aoc18.22a.txt";
+        List<String> inputAsStrings = utilities.getFileContentsAsStrings(filename);
+        char[][] caveSystem = utilities.convertInputToMap(inputAsStrings);
+
+        Point target = new Point(10, 10);
+        int actual = day22.calculateRiskLevel(caveSystem, target);
+
+        Assertions.assertEquals(114, actual);
+    }
 }
