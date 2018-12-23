5205e2374b8620ae431392bf351b879014c9eaf8
Paul Moore <pdmoore@gmail.com>
2018-12-22 17:28:38 -0800

    day 22 solution 1

--- src/day22.java
+++ src/day22.java
@@ -1,23 +1,107 @@
 import java.awt.*;
+import java.math.BigInteger;
 
 public class day22 {
 
-    public static final char ROCKY  = '.';
-    public static final char WET    = '=';
+    public static final char ROCKY = '.';
+    public static final char WET = '=';
     public static final char NARROW = '|';
 
-    
+    public static final BigInteger bi3 = new BigInteger(String.valueOf(3));
+    public static final BigInteger biMultY = new BigInteger(String.valueOf(16807));
+    public static final BigInteger biMultX = new BigInteger(String.valueOf(48271));
+
+    public static final BigInteger elMod = new BigInteger(String.valueOf(20183));
+
+
     public static int calculateRiskLevel(char[][] caveSystem, Point target) {
         int riskLevel = 0;
         for (int y = 0; y <= target.y; y++) {
             for (int x = 0; x <= target.x; x++) {
-                switch (caveSystem[x][y]) {
-                    case WET:    riskLevel += 1; break;
-                    case NARROW: riskLevel += 2; break;
+                switch (caveSystem[y][x]) {
+                    case WET:
+                        riskLevel += 1;
+                        break;
+                    case NARROW:
+                        riskLevel += 2;
+                        break;
                 }
             }
         }
 
         return riskLevel;
+    }
+
+    public static char[][] solution1_caveSystem(Region[][] regions, int depth, Point target) {
+        BigInteger biDepth = new BigInteger(String.valueOf(depth));
+
+        for (int x = 0; x <= target.x; x++) {
+            for (int y = 0; y <= target.y; y++) {
+
+                BigInteger geologicIndex = null;
+                if ((x == 0 && y == 0) ||
+                        (x == target.x && y == target.y)) {
+                    geologicIndex = new BigInteger(String.valueOf(0));
+                } else if (y == 0) {
+                    BigInteger biX = new BigInteger(String.valueOf(x));
+                    geologicIndex = biMultY.multiply(biX);
+                } else if (x == 0) {
+                    BigInteger biX = new BigInteger(String.valueOf(y));
+                    geologicIndex = biMultX.multiply(biX);
+                } else {
+                    geologicIndex = regions[x - 1][y].erosionLevel.multiply(regions[x][y - 1].erosionLevel);
+                }
+
+                Region region = new Region(geologicIndex, biDepth);
+
+                regions[x][y] = region;
+            }
+        }
+
+        return caveSystemOf(regions);
+    }
+
+    private static char[][] caveSystemOf(Region[][] regions) {
+        char[][] caveSystem = new char[regions[0].length][regions.length];
+
+        for (int x = 0; x < regions.length; x++) {
+            for (int y = 0; y < regions[0].length; y++) {
+                caveSystem[y][x] = regions[x][y].type;
+            }
+        }
+
+        return caveSystem;
+    }
+
+    public static int solution1(Region[][] regions, int depth, Point target) {
+        char[][] caveSystem = solution1_caveSystem(regions, depth, target);
+        return calculateRiskLevel(caveSystem, target);
+    }
+
+    public static class Region {
+        BigInteger erosionLevel;
+        BigInteger geologicIndex;
+        char type;
+
+        public Region(BigInteger geologicIndex, BigInteger biDepth) {
+            this.geologicIndex = geologicIndex;
+            this.erosionLevel = geologicIndex.add(biDepth).mod(elMod);
+            assignType();
+        }
+
+        public void assignType() {
+            int mod = erosionLevel.mod(bi3).intValue();
+            switch (mod) {
+                case 0:
+                    type = ROCKY;
+                    break;
+                case 1:
+                    type = WET;
+                    break;
+                case 2:
+                    type = NARROW;
+                    break;
+            }
+        }
     }
 }
--- test/day22Tests.java
+++ test/day22Tests.java
@@ -1,12 +1,11 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
 import java.util.List;
 
 public class day22Tests {
-
-
 
     @Test
     public void calculateRiskLevel() {
@@ -19,4 +18,45 @@ public class day22Tests {
 
         Assertions.assertEquals(114, actual);
     }
+
+    @Test
+    public void solution1_example_caveSystem() {
+        int depth = 510;
+        Point target = new Point(10, 10);
+
+        day22.Region[][] regions = new day22.Region[target.x + 1][target.y + 1];
+
+        String filename = "data/aoc18.22b.txt";
+        List<String> inputAsStrings = utilities.getFileContentsAsStrings(filename);
+        char[][] expected = utilities.convertInputToMap(inputAsStrings);
+
+        char[][] actual = day22.solution1_caveSystem(regions, depth, target);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void solution1_example() {
+        int depth = 510;
+        Point target = new Point(10, 10);
+
+        day22.Region[][] regions = new day22.Region[target.x + 1][target.y + 1];
+
+        int actual = day22.solution1(regions, depth, target);
+
+        Assertions.assertEquals(114, actual);
+    }
+
+    @Test
+    public void solution1() {
+        int depth = 11541;
+        Point target = new Point(14, 778);
+
+        day22.Region[][] regions = new day22.Region[target.x + 1][target.y + 1];
+
+        int actual = day22.solution1(regions, depth, target);
+
+        Assertions.assertEquals(11575, actual);
+    }
+
 }
