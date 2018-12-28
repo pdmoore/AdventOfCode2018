e9b8e51774e0a64516fa34f11b1312478707432c
Paul Moore <pdmoore@gmail.com>
2018-12-28 15:14:21 -0800

    day 6 - refactor board logic

--- src/day6.java
+++ src/day6.java
@@ -13,9 +13,12 @@ public class day6 {
 
         // for each point in grid, determine closest neighbor
 
+        board.fillClosestCoordinates();
+
+
+        board.printGrid();
 
         // calculate the result, ignoring points on infinite edge
-
         return board.countOfLargestArea();
     }
 
@@ -42,10 +45,13 @@ public class day6 {
 
     private static class Board {
 
+        private final List<Point> initialCoordinates;
         // TODO likely to change since input exceeds 26 letters, and may want to track an object grid
         char[][] grid;
 
         public Board(List<Point> points) {
+
+            initialCoordinates = points;
 
             int maxRow = locateMaxRow(points);
             int maxCol = locateMaxCol(points);
@@ -53,13 +59,13 @@ public class day6 {
             grid = new char[maxRow + 2][maxCol + 2];
 
             fillStartingGrid(grid, points);
-
-            fillClosestCoordinates(grid, points);
-
-            printGrid(grid);
         }
 
-        private void fillClosestCoordinates(char[][] grid, List<Point> points) {
+        public void fillClosestCoordinates() {
+            fillClosestCoordinates(grid, initialCoordinates);
+        }
+
+        void fillClosestCoordinates(char[][] grid, List<Point> points) {
 
             //Brute force (ok for small grid and few points)
             for (int row = 0; row < numRows(grid); row++) {
@@ -89,7 +95,7 @@ public class day6 {
             }
         }
 
-        private void printGrid(char[][] grid) {
+        void printGrid() {
             for (int row = 0; row < numRows(grid); row++) {
                 for (int col = 0; col < numCol(grid); col++) {
                     System.out.print(grid[row][col]);
